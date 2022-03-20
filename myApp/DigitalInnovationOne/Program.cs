a5654db9b6ba7fde5a460cd636ea90f555bf71b9
Leticia Sabbath <leticiasbh@gmail.com>
2022-03-20 11:50:01 -0300

    Teste primeiro programa

--- myApp/DigitalInnovationOne/Program.cs
+++ myApp/DigitalInnovationOne/Program.cs
@@ -7,6 +7,12 @@ namespace DigitalInnovationOne
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int vezes = 5;
+
+           for (int i = 0; i < vezes; i++) {
+               Console.WriteLine($" Bem-vindo(a) ao curso de .NET{i}");
+           }
         }
     }
 }
